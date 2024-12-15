c5b511b47496767f8df94bbc2ddebd7f85cbbf4f
Zhichen Zeng <zczeng@cs.washington.edu@kiska.cs.washington.edu>
2024-12-15 03:24:17 -0800

    group_2 results 12_15

--- fa24/sorting-project/group2/kernel.cpp
+++ fa24/sorting-project/group2/kernel.cpp
@@ -9,117 +9,102 @@ typedef struct {
     uint32_t key;
     uint32_t value;
 } ValueKeyPair;
-#define BITS_PER_PASS 4  // Sort by 4 bits at a time
+#define BITS_PER_PASS 8  // Sort by 4 bits at a time
 #define NUM_BUCKETS (1 << BITS_PER_PASS)  // 16 buckets for 4 bits
 #define MAX_BITS 32  // Sorting 32-bit integers
 // Local histogram
-__attribute__((section(".dram"))) int local_count[NUM_BUCKETS * bsg_tiles_X * bsg_tiles_Y];
-__attribute__((section(".dram"))) int prefix_sum[NUM_BUCKETS];
-__attribute__((section(".dram"))) int prefix_sum_buffer[NUM_BUCKETS];
-void blellochScan(int *d_array, int size) {
-  int block_size = bsg_tiles_X * bsg_tiles_Y;
-  int tid = __bsg_id;
-  if (__bsg_id == 0) {
-    for (int i = 0; i < size; i++) {
-      prefix_sum_buffer[i] = d_array[i];
-    }
-  }
-  bsg_barrier_hw_tile_group_sync();
-  // Upsweep
-  for (int step = 1; step < size; step *= 2) {
-    int index = (tid + 1) * step * 2 - 1;
-    if (index < size) {
-      prefix_sum_buffer[index] += prefix_sum_buffer[index - step];
-    }
-    bsg_barrier_hw_tile_group_sync();
-  }
-  // Clear the last element to prepare for downsweep
-  if (tid == (block_size) - 1) {
-      prefix_sum_buffer[size - 1] = 0;
-  }
-  bsg_barrier_hw_tile_group_sync();
-  // Downsweep phase
-  for (int step = size / 2; step > 0; step /= 2) {
-      int index = (tid + 1) * step * 2 - 1;
-      if (index < size) {
-          int temp = prefix_sum_buffer[index];
-          prefix_sum_buffer[index] += prefix_sum_buffer[index - step];
-          prefix_sum_buffer[index - step] = temp;
-      }
-      bsg_barrier_hw_tile_group_sync();
-  }
-  // Write back to global memory
-  if (__bsg_id == 0) {
-    for (int i = 0; i < size; i += 1) {
-      d_array[i] = prefix_sum_buffer[i];
-    }
-  }
-}
-void warmup(ValueKeyPair *d_array, int size) {
-    int tid = __bsg_id;
-    int block_size = bsg_tiles_X * bsg_tiles_Y;
-    // Warm up the cache by accessing the elements
-    for (int i = tid; i < size; i += block_size) {
-        uint32_t value = d_array[i].value;
-        asm volatile ("" : : "r" (value) : "memory");
-    }
-    bsg_barrier_hw_tile_group_sync();
-}
+__attribute__((section(".dram"))) int local_count[bsg_tiles_X * bsg_tiles_Y][NUM_BUCKETS];
+__attribute__((section(".dram"))) int global_prefix_sum [NUM_BUCKETS];
 void radixSort(ValueKeyPair *d_array, ValueKeyPair *d_buffer, int size, int bit_offset) {
     int tid = __bsg_id;
     int block_size = bsg_tiles_X * bsg_tiles_Y;
+    int data_per_core = size / block_size;
+    int start = tid * data_per_core;
+    int end = start + data_per_core;
+    register int local_count_reg[NUM_BUCKETS] = {0};
+    register int local_prefix_sum_reg[NUM_BUCKETS]= {0};
+    register int local_prefix_sum_buffer_reg[NUM_BUCKETS]= {0};
+    register int global_prefix_sum_reg[NUM_BUCKETS];
     // Initialize local count
-    for (int i = tid; i < NUM_BUCKETS * block_size; i += block_size) {
-        local_count[i] = 0;
+    for (int bucket = 0; bucket < NUM_BUCKETS; bucket++)
+    {
+        local_count[tid][bucket] = 0;
     }
     bsg_barrier_hw_tile_group_sync();
-    // Build histogram for this digit
+    for (int i = __bsg_id; i < block_size; i += block_size)
+    {
+        for (int j = start; j < end; j++)
+        {
+            uint32_t value = d_array[j].value;
+            int bucket = (value>> bit_offset) & (NUM_BUCKETS - 1);
+            local_count_reg[bucket]++;
+        }
+    }
+    bsg_barrier_hw_tile_group_sync();
+        // Write local counts to global memory
+    for (int i = __bsg_id; i < block_size; i += block_size)
+    {
+        for (int j= 0; j<NUM_BUCKETS; j++)
+        {
+            // prefix_sum[i][i]=0;
+            local_count[i][j] = local_count_reg[j];
+        }
+    }
+    bsg_barrier_hw_tile_group_sync();
+    for (int i = __bsg_id; i < block_size; i += block_size)
+    {
+        for (int j = 0; j < tid; j++)
+        {
+            for (int k = 0; k < NUM_BUCKETS; k++)
+            {
+                local_prefix_sum_reg[k] +=local_count[j][k] ;
+            }
+        }
+    }
+    bsg_barrier_hw_tile_group_sync();
+    if (__bsg_id == block_size-1)
+    {
+        global_prefix_sum[0]=0;
+        int shift = 0;
+        for (int i = 0; i < NUM_BUCKETS; i++)
+        {
+            shift += local_prefix_sum_reg[i]+local_count_reg[i];
+            global_prefix_sum[i+1]=shift;
+        }
+    }
+    bsg_barrier_hw_tile_group_sync();
+    for (int i = __bsg_id; i < block_size; i += block_size)
+    {
+        for (int j = 0; j < NUM_BUCKETS; j++)
+        {
+            global_prefix_sum_reg[j]=global_prefix_sum[j];
+        }
+    }
+    bsg_barrier_hw_tile_group_sync();
+        for (int i = __bsg_id; i < block_size; i += block_size)
+    {
+        for (int j = start; j < end; j++)
+        {
+            uint32_t value = d_array[j].value;
+            int bucket = (value>> bit_offset) & (NUM_BUCKETS - 1);
+            int pos = local_prefix_sum_buffer_reg[bucket] + local_prefix_sum_reg[bucket] + global_prefix_sum_reg[bucket];
+            d_buffer[pos] = d_array[j];
+            local_prefix_sum_buffer_reg[bucket]++;
+        }
+    }
+    bsg_barrier_hw_tile_group_sync();
     for (int i = __bsg_id; i < size; i += block_size) {
-        uint32_t value = d_array[i].value;
-        int digit = __bsg_id + block_size * ((value >> bit_offset) & (NUM_BUCKETS - 1));
-        bsg_amoadd(&local_count[digit], 1);
-    }
-    bsg_barrier_hw_tile_group_sync();
-    // Perform an exclusive scan (prefix sum) on the histogram
-    for (int i = tid; i < NUM_BUCKETS; i += block_size) {
-      prefix_sum[i] = 0;
-    }
-    bsg_barrier_hw_tile_group_sync();
-    // Sum up for different tiles
-    for (int bucket = 0; bucket < NUM_BUCKETS; bucket++) {
-      int added_occurence = local_count[tid + bucket * block_size];
-      bsg_amoadd(&prefix_sum[bucket], added_occurence);
-    }
-    bsg_barrier_hw_tile_group_sync();
-    // Scan for prefix sum
-    blellochScan(prefix_sum, NUM_BUCKETS);
-    // Rearrange elements based on the prefix sum
-    if (__bsg_id == 0) {
-      for (int i = 0; i < size; i += 1) {
-        uint32_t value = d_array[i].value;
-        int digit = (value >> bit_offset) & (NUM_BUCKETS - 1);
-        d_buffer[prefix_sum[digit]] = d_array[i];
-        bsg_amoadd(&prefix_sum[digit], 1);
-      }
-    }
-    bsg_barrier_hw_tile_group_sync();
-    // Copy sorted data back to the original array
-    if (__bsg_id == 0) {
-      for (int i = 0; i < size; i += 1) {
         d_array[i] = d_buffer[i];
-      }
     }
+    bsg_barrier_hw_tile_group_sync();
 }
 extern "C" __attribute__ ((noinline))
-int kernel_sort_radix(ValueKeyPair * Unsorted, ValueKeyPair * Sorted, int N) {
+int kernel_sort_radix(ValueKeyPair * Unsorted, ValueKeyPair * Sorted,int N) {
   bsg_barrier_hw_tile_group_init();
   bsg_barrier_hw_tile_group_sync();
   bsg_cuda_print_stat_kernel_start();
-  // Warm up the cache
-  warmup(Unsorted, N);
-  bsg_barrier_hw_tile_group_sync();
   for (int bit = 0; bit < 32; bit += BITS_PER_PASS) {
-      radixSort(Unsorted, Sorted, N, bit);
+      radixSort(Unsorted, Sorted,N, bit );
       bsg_barrier_hw_tile_group_sync();
   }
   bsg_fence();
--- fa24/sorting-project/group2/template.mk
+++ fa24/sorting-project/group2/template.mk
@@ -14,7 +14,7 @@
 include parameters.mk
 include app_path.mk
 HB_HAMMERBENCH_PATH:=$(shell git rev-parse --show-toplevel)
-override BSG_MACHINE_PATH = $(REPLICANT_PATH)/machines/pod_X1Y1_ruche_X4Y2_hbm_one_pseudo_channel 
+override BSG_MACHINE_PATH = $(REPLICANT_PATH)/machines/pod_X1Y1_ruche_X$(tile-x)Y$(tile-y)_hbm_one_pseudo_channel 
 include $(HB_HAMMERBENCH_PATH)/mk/environment.mk
 
 ###############################################################################
@@ -25,8 +25,8 @@ include parameters.mk
 include app_path.mk
 
 # Tile Group Dimensions
-TILE_GROUP_DIM_X ?= 4
-TILE_GROUP_DIM_Y ?= 2
+TILE_GROUP_DIM_X = $(tile-x)
+TILE_GROUP_DIM_Y = $(tile-y)
 
 vpath %.c   $(APP_PATH)
 vpath %.cpp $(APP_PATH)
