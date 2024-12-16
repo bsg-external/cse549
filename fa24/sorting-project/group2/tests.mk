367253277fe1cd6e0a3ecb5310f7a2bedeb07b07
Zhichen Zeng <108505471+Zhichenzzz@users.noreply.github.com>
2024-12-16 11:21:52 +0800

    Group 2 Update (#2)
    Co-authored-by: Zhichen Zeng <zczeng@cs.washington.edu@kiska.cs.washington.edu>

--- fa24/sorting-project/group2/tests.mk
+++ fa24/sorting-project/group2/tests.mk
@@ -1,19 +1,22 @@
 #########################################################
-# TESTS += $(call test-name,[buffer-size]) #
-# #########################################################
+# TESTS += $(call test-name,tile_X,tile_Y,[buffer-size]) #
+#########################################################
+TESTS += $(call test-name,4,2,4096)
+TESTS += $(call test-name,4,2,8192)
+TESTS += $(call test-name,4,2,16384)
 
+TESTS += $(call test-name,4,4,4096)
+TESTS += $(call test-name,4,4,8192)
+TESTS += $(call test-name,4,4,16384)
 
-TESTS += $(call test-name,64)
-TESTS += $(call test-name,128)
-TESTS += $(call test-name,256)
-TESTS += $(call test-name,512)
-TESTS += $(call test-name,1024)
-TESTS += $(call test-name,2048)
-TESTS += $(call test-name,4096)
-TESTS += $(call test-name,8192)
-# TESTS += $(call test-name,16384)
-# TESTS += $(call test-name,65536)
-# TESTS += $(call test-name,262144)
-# TESTS += $(call test-name,1048576)
-# TESTS += $(call test-name,4194304)
-# TESTS += $(call test-name,16777216)
+TESTS += $(call test-name,8,4,4096)
+TESTS += $(call test-name,8,4,8192)
+TESTS += $(call test-name,8,4,16384)
+
+TESTS += $(call test-name,8,8,4096)
+TESTS += $(call test-name,8,8,8192)
+TESTS += $(call test-name,8,8,16384)
+
+TESTS += $(call test-name,16,8,4096)
+TESTS += $(call test-name,16,8,8192)
+TESTS += $(call test-name,16,8,16384)
